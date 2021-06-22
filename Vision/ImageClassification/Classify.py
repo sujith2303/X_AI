75e237e570a9fd327712de3f2b3bcbe58fbceb47
Anumala Sujith <71480231+sujith2303@users.noreply.github.com>
2021-06-22 10:50:57 +0530

    Update Classify.py

--- Vision/ImageClassification/Classify.py
+++ Vision/ImageClassification/Classify.py
@@ -41,22 +41,23 @@ class ImageClassification:
     return model
   
   def plot_results(self):
-      print('Plotting accuracy.................')
-      plt.plot(history.history['accuracy'])
-      plt.plot(history.history['val_accuracy'])
-      plt.title('model accuracy')
-      plt.ylabel('accuracy')
-      plt.xlabel('epoch')
-      plt.legend(['train', 'test'], loc='upper left')
-      plt.show()
-      print('Plotting loss.................')
-      plt.plot(history.history['loss'])
-      plt.plot(history.history['val_loss'])
-      plt.title('model loss')
-      plt.ylabel('loss')
-      plt.xlabel('epoch')
-      plt.legend(['train', 'test'], loc='upper left')
-      plt.show()
+    history = self.history
+    print('Plotting accuracy.................')
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    print('Plotting loss.................')
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
-      
-    
+
+
