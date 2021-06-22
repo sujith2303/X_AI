fb7f835a52bb42aa620ac1a1700637d5a53289c2
Anumala Sujith <71480231+sujith2303@users.noreply.github.com>
2021-06-22 10:50:13 +0530

    Update Classify.py

--- Vision/ImageClassification/Classify.py
+++ Vision/ImageClassification/Classify.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import tqdm
 import os
+import matplotlib.pyplot as plt
 
 __author__ = 'Sujith Anumala'
 
@@ -12,6 +13,7 @@ class ImageClassification:
     self.classes   = classes
     self.images    = None
     self.labels    = None
+    self.history   = None
     if not ImagePath:
       raise FileNotFoundError('Enter a valid path')
     if not LabelsPath:
@@ -19,17 +21,42 @@ class ImageClassification:
     if not num_classes:
       raise ValueError('Enter a Non-zero number')
   
-  def Train(self,epochs=10,input_shape=(320,320,3):
+  def Train(self,epochs=10,input_shape=(320,320,3),batch_size = 128):
     
     model =tf.keras.applications.vgg16.VGG16(
-    include_top=True, weights='imagenet',input_shape=input_shape, classes=self.classes,
-    classifier_activation='softmax')
+    include_top=False, weights='imagenet')
+    
+    x_input = tf.keras.layers.Input(shape = input_shape)
+    x = model(x_input,training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(100,activation='relu')(x_input)
+    output=tf.keras.layers.Dense(self.classes,activation='Softmax')(x)
+    
+    model = tf.keras.Model(x_input,output,name='Classification Model')
     
     model.compile(loss='mse',optimizer='adam',metrics=['Accuracy'])
-    history = model.fit(x=trainx,y=trainy,epochs)
+    
+    history = model.fit(x=trainx,y=trainy,epochs,batch_size=batch_size)
+    
     return model
-    
-    
+  
+  def plot_results(self):
+      print('Plotting accuracy.................')
+      plt.plot(history.history['accuracy'])
+      plt.plot(history.history['val_accuracy'])
+      plt.title('model accuracy')
+      plt.ylabel('accuracy')
+      plt.xlabel('epoch')
+      plt.legend(['train', 'test'], loc='upper left')
+      plt.show()
+      print('Plotting loss.................')
+      plt.plot(history.history['loss'])
+      plt.plot(history.history['val_loss'])
+      plt.title('model loss')
+      plt.ylabel('loss')
+      plt.xlabel('epoch')
+      plt.legend(['train', 'test'], loc='upper left')
+      plt.show()
 
       
     
