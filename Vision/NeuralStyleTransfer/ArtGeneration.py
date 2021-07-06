071141a8b2a3230625fa996c41082fb51cb7f95a
Anumala Sujith <71480231+sujith2303@users.noreply.github.com>
2021-07-06 19:11:41 +0530

    Update ArtGeneration.py

--- Vision/NeuralStyleTransfer/ArtGeneration.py
+++ Vision/NeuralStyleTransfer/ArtGeneration.py
@@ -9,6 +9,7 @@ class ArtGeneration:
     self.img_nrows=img_shape[0]
     self.img_ncols=img_shape[1]
     self.model =None
+    self.iteration = 1000
     self.style_layer_names = [
         "block1_conv1",
         "block2_conv1",
@@ -24,9 +25,7 @@ class ArtGeneration:
     if not style_image_path:
       raise FileNotFoundError('Please Enter a valid path for style_image!')
     baseimage = cv2.imread(image_path)
-    baseimage = cv2.resize(baseimage,img_shape[:2])
     styleimage =cv2.imread(style_image_path)
-    styleimage =cv2.resize(styleimage,img_shape[:2])
     print('Base image')
     cv2.imshow(baseimage)
     print('Style image')
@@ -101,63 +100,32 @@ class ArtGeneration:
     # Add total variation loss
     loss += total_variation_weight * total_variation_loss(combination_image)
     return loss
-    
-    
-    
-    
-    
-    
-    
   
-  def content_cost(self,a_C,a_G):
-    m, n_H, n_W, n_C = a_G.shape
-    new_shape = [int(m), int(n_H * n_W), int(n_C)]
-    a_C_unrolled = tf.reshape(a_C, new_shape)
-    a_G_unrolled = tf.reshape(a_G, new_shape)
-    J_content = (.25 / float(int(n_H * n_W * n_C))) * tf.reduce_sum(np.power(a_G_unrolled - a_C_unrolled, 2))
-    return J_content
-  
-  def grammatrix(self,A):
-    GA = tf.matmul(A, tf.transpose(A)) 
-    return GA
+  @tf.function
+  def compute_loss_and_grads(self,combination_image, base_image, style_reference_image):
+      with tf.GradientTape() as tape:
+          loss = self.compute_loss( base_image, style_reference_image,combination_image)
+      grads = tape.gradient(loss, combination_image)
+      return loss, grads  
 
-  def style_cost(self,a_S,a_G):
-    m, n_H, n_W, n_C = a_G.get_shape().as_list()
-    a_S = tf.reshape(a_S, [n_H * n_W, n_C])
-    a_S = tf.transpose(a_S)
-    a_G = tf.reshape(a_G, [n_H * n_W, n_C])
-    a_G = tf.transpose(a_G)
-    GS = gram_matrix(a_S)
-    GG = gram_matrix(a_G)
-    factor = (.5 / (n_H * n_W * n_C)) ** 2
-    J_style_layer = factor * tf.reduce_sum(np.power(GS - GG, 2))
-    return J_style_layer
-  
-  def compute_style_cost(self,model,style_layers):
-    J_style = 0
-    for layer_name, coeff in STYLE_LAYERS:
-        out = model[layer_name]
-        a_S = sess.run(out)
-        a_G = out
-        J_style_layer = compute_layer_style_cost(a_S, a_G)
-        J_style += coeff * J_style_layer
-    return J_style
-  
-  def total_cost(self,J_content, J_style, alpha = 10, beta = 40):
-    J = alpha * J_content + beta * J_style
-    return J
-  def model_nn(self,sess, input_image, num_iterations = 200):
-    sess.run(tf.global_variables_initializer())
-    sess.run(model['input'].assign(input_image))
-    for i in range(num_iterations):
-        somthing = sess.run(train_step)
-        generated_image = sess.run(model['input'])
-        if i%20 == 0:
-            Jt, Jc, Js = sess.run([J, J_content, J_style])
-            print("Iteration " + str(i) + " :")
-            print("total cost = " + str(Jt))
-            print("content cost = " + str(Jc))
-            print("style cost = " + str(Js))
-            save_image("output/" + str(i) + ".png", generated_image)
-    save_image('output/generated_image.jpg', generated_image)
-    return generated_image
+  def main(self):
+    optimizer = keras.optimizers.SGD(
+    keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=100.0, decay_steps=100, decay_rate=0.96
+    )
+)
+
+    base_image = self.preprocess_image(self.base_image_path)
+    style_reference_image = self.preprocess_image(self.style_reference_image_path)
+    combination_image = tf.Variable(self.preprocess_image(self.base_image_path))
+    for i in range(1, self.iterations + 1):
+        loss, grads = self.compute_loss_and_grads(
+            combination_image, base_image, style_reference_image
+        )
+        optimizer.apply_gradients([(grads, combination_image)])
+        if i % 100 == 0:
+            print("Iteration %d: loss=%.2f" % (i, loss))
+            img = deprocess_image(combination_image.numpy())
+            fname = result_prefix + "_at_iteration_%d.png" % i
+            keras.preprocessing.image.save_img(fname, img)
+            
